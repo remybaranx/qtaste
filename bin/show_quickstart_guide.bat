01d66101fac2d70bdb2d5736487d5d5a9bd17365
Remy Baranx <remy.bricol@gmail.com>
2015-04-20 11:59:12 +0200

    BUG#15: show_quickstart_guide.* scripts were useless

--- bin/show_quickstart_guide.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set QTASTE_ROOT=%~dp0\..
-
-start %QTASTE_ROOT%\doc\pdf\quickstart_guide.pdf
--- bin/show_quickstart_guide.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xdg-open $(dirname "$0")/../doc/pdf/quickstart_guide.pdf
-
